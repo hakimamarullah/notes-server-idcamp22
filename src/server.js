a9222545483b848cb0252cee5eb86abc5468a393
hakimamarullah <hakimaamarullah@gmail.com>
2022-06-08 17:28:25 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const init = async () => {
 
     const config = {
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
