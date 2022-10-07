a32b239c3b034a395e41d6bada3a924fc7589730
Capt. Hakim Amarullah <61151625+hakimamarullah@users.noreply.github.com>
2022-10-07 14:33:31 +0700

    Feat: Collaboration implemented (#5)

--- src/api/user/routes.js
+++ src/api/user/routes.js
@@ -9,6 +9,12 @@ const routes = (handler) => [
     path: '/users/{id}',
     handler: handler.getUserByIdHandler,
   },
+  {
+    method: 'GET',
+    path: '/users',
+    handler: handler.getUsersByUsernameHandler,
+  },
+
 ];
 
 module.exports = routes;
